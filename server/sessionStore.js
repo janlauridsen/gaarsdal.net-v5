c2c502dfbceb0bea3ccd4188e235ce49a2540769
Jan Lauridsen <jan.lauridsen@hotmail.com>
2025-12-18 14:49:44 +0100

    Create sessionStore.js

---
+++ server/sessionStore.js
@@ -0,0 +1,22 @@
+import { v4 as uuid } from "uuid";
+
+const TTL = 1000 * 60 * 60 * 24 * 14;
+const sessions = new Map();
+
+export function getSession(id, ip) {
+  let s = id && sessions.get(id);
+
+  if (!s || Date.now() - s.last > TTL) {
+    s = {
+      id: uuid(),
+      ip,
+      created: Date.now(),
+      last: Date.now(),
+      log: []
+    };
+    sessions.set(s.id, s);
+  }
+
+  s.last = Date.now();
+  return s;
+}
