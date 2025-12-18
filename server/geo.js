10b7265019ccfd2eff0e5c4be7c0622ad6b10f7d
Jan Lauridsen <jan.lauridsen@hotmail.com>
2025-12-18 15:59:51 +0100

    Create geo.js

---
+++ server/geo.js
@@ -0,0 +1,7 @@
+export function resolveGeo(req) {
+  return {
+    country: req.headers["x-vercel-ip-country"] || null,
+    region: req.headers["x-vercel-ip-country-region"] || null,
+    city: req.headers["x-vercel-ip-city"] || null
+  };
+}
