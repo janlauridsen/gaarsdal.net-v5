f72f1967bde0631e2a3f6f4e0a2ba5061a8f0c3a
Jan Lauridsen <jan.lauridsen@hotmail.com>
2025-12-18 20:22:49 +0100

    Update index.js

--- server/prompts/index.js
+++ server/prompts/index.js
@@ -1,10 +1,10 @@
-import state_0_v3 from "./state_0_v3.md";
-import state_1_v3 from "./state_1_v3.md";
-import state_4_v3 from "./state_4_v3.md";
-import state_5_v3 from "./state_5_v3.md";
-import state_7_v3 from "./state_7_v3.md";
-import state_8_v2 from "./state_8_v2.md";
-import guard_v2 from "./guard_v2.md";
+import state_0_v3 from "./state_0_v3.js";
+import state_1_v3 from "./state_1_v3.js";
+import state_4_v3 from "./state_4_v3.js";
+import state_5_v3 from "./state_5_v3.js";
+import state_7_v3 from "./state_7_v3.js";
+import state_8_v2 from "./state_8_v2.js";
+import guard_v2 from "./guard_v2.js";
 
 export const prompts = {
   state_0_v3,
