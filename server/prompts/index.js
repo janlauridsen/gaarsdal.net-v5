f4e599a7c44cf1241e6807bb41964bb55b78ea4d
Jan Lauridsen <jan.lauridsen@hotmail.com>
2025-12-18 19:13:27 +0100

    Create index.js

---
+++ server/prompts/index.js
@@ -0,0 +1,17 @@
+import state_0_v3 from "./state_0_v3.md";
+import state_1_v3 from "./state_1_v3.md";
+import state_4_v3 from "./state_4_v3.md";
+import state_5_v3 from "./state_5_v3.md";
+import state_7_v3 from "./state_7_v3.md";
+import state_8_v2 from "./state_8_v2.md";
+import guard_v2 from "./guard_v2.md";
+
+export const prompts = {
+  state_0_v3,
+  state_1_v3,
+  state_4_v3,
+  state_5_v3,
+  state_7_v3,
+  state_8_v2,
+  guard_v2
+};
