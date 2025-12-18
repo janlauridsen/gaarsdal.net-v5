1e14ff43f56b749a676540d843f6bfd7aa00b28e
Jan Lauridsen <jan.lauridsen@hotmail.com>
2025-12-18 21:22:11 +0100

    Delete server/xx_promptLoader.js

--- server/xx_promptLoader.js
+++
@@ -1,9 +0,0 @@
-import fs from "fs";
-import path from "path";
-
-const base = path.resolve("server/prompts");
-
-export function loadPrompt(prompt_id) {
-  const file = path.join(base, `${prompt_id}.md`);
-  return fs.readFileSync(file, "utf8");
-}
