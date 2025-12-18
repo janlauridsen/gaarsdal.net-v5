e2d57089b00b1d4317dbfdca0534c1f7ebcc3c16
Jan Lauridsen <jan.lauridsen@hotmail.com>
2025-12-18 15:14:57 +0100

    Update anomalyRules.js

--- server/anomalyRules.js
+++ server/anomalyRules.js
@@ -2,28 +2,13 @@ import { Anomaly } from "./enums.js";
 
 export function detectAnomalies(text) {
   const t = text.toLowerCase();
-  const anomalies = [];
+  const a = [];
 
-  if (/(jeg forstår|det lyder|jeg er ked af)/.test(t)) {
-    anomalies.push(Anomaly.RELATIONAL_TONE);
-  }
+  if (/(jeg forstår|det lyder)/.test(t)) a.push(Anomaly.RELATIONAL_TONE);
+  if (/(du bør|jeg anbefaler)/.test(t)) a.push(Anomaly.ADVICE_FOUND);
+  if (/\?$/.test(text.trim())) a.push(Anomaly.QUESTION_FOUND);
+  if (/(jeg håber|det er normalt)/.test(t)) a.push(Anomaly.EMPATHY_FOUND);
+  if (text.length > 700) a.push(Anomaly.OVERLONG_RESPONSE);
 
-  if (/(du bør|jeg anbefaler|mit råd)/.test(t)) {
-    anomalies.push(Anomaly.ADVICE_FOUND);
-  }
-
-  if (/\?$/.test(text.trim())) {
-    anomalies.push(Anomaly.QUESTION_FOUND);
-    anomalies.push(Anomaly.INVITATION_FOUND);
-  }
-
-  if (/(jeg håber|det er helt normalt)/.test(t)) {
-    anomalies.push(Anomaly.EMPATHY_FOUND);
-  }
-
-  if (text.length > 700) {
-    anomalies.push(Anomaly.OVERLONG_RESPONSE);
-  }
-
-  return anomalies;
+  return a;
 }
