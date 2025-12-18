c67b5ca5b2d140123fe2ccece06b1bfa2ac65fe2
Jan Lauridsen <jan.lauridsen@hotmail.com>
2025-12-18 20:19:19 +0100

    Update and rename state_1_v3.md to state_1_v3.js

--- server/prompts/state_1_v3.js
+++ server/prompts/state_1_v3.js
@@ -1,6 +1,8 @@
+export default `
 Dette værktøj giver kun faglig afklaring.
 
 Det yder ikke rådgivning, behandling eller personlige vurderinger.
 Det forholder sig ikke til individuelle valg eller behov.
 
 Dette er værktøjets ramme.
+`;
